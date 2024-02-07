4c1ea8668f0b6b34127d57104d506067e07654af
Erika <erikavianac@gmail.com>
2024-02-07 13:50:27 +0000

    s

--- src/components/explore/card.tsx
+++ src/components/explore/card.tsx
@@ -1,8 +1,9 @@
-'use client';
+"use client";
 
-import { AnimatedVerticalComponent } from '@/components/utils/animatedVertical';
-import { ImageComponent } from '@/components/utils/image';
-import { motion } from 'framer-motion';
+import { AnimatedVerticalComponent } from "@/components/utils/animatedVertical";
+import { ImageComponent } from "@/components/utils/image";
+import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 interface ExploreCardProps {
   alt: string;
@@ -17,13 +18,20 @@ export function ExploreCardComponent({
   src,
   title,
 }: ExploreCardProps) {
+  const imgDivRef = useRef<any>(null);
+
   return (
-    <div className="opacity-[1.5] relative w-[370px] h-[400px] md:w-[400px] md:h-[400px] flex justify-center items-center text-[24px]  tracking-[0.25rem] md:hover:scale-105 duration-300 md:brightness-75 active:scale-95 hover:brightness-110  cursor-pointer rounded-md overflow-hidden">
-      <div className="bg-black/30 w-full h-full absolute z-30"/>
+    <div
+      ref={imgDivRef}
+      className="opacity-[1.5] relative w-full min-w-[390px] max-w-[95%] w-max-[600px] h-[400px] md:w-[400px] md:h-[400px] 
+    flex justify-center items-center text-[24px]  tracking-[0.25rem] md:hover:scale-105 duration-300 md:brightness-75 
+    active:scale-95 hover:brightness-110  cursor-pointer rounded-md overflow-hidden"
+    >
+      <div className="bg-black/30 w-full h-full absolute z-30 " />
       <ImageComponent
         alt={alt}
-        h={'h-full'}
-        w={'w-full'}
+        h={"h-full"}
+        w={"w-full"}
         src={`${src}`}
         containerClassname={containerClassname}
       />
