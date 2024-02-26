2ef0df5c84b19a146a212b87d300720a0f51cd61
Erika <erikavianac@gmail.com>
2024-02-26 12:46:50 +0000

    s

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,7 +6,7 @@ export default function robots(): MetadataRoute.Robots {
         {
             userAgent: "*",
             allow: "/",
-            disallow: ["/orcamentoById", "privacy"],
+            disallow: ["/orcamentoById"],
         }
     ],
     sitemap: `${process.env.BASE_URL}/sitemap.xml`
