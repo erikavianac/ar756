5426fda363df4bba0f60ef78d92c6165bf902e00
igorlabaki <igorlabakig@gmail.com>
2024-03-04 00:03:40 +0000

    s

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@
 export default function NotFound() {
   return (
     <div className={`flex flex-col items-center justify-center w-full min-h-screen px-2 bg-faixada flex-1 h-screen`}>
-        <h1 className="text-3xl fon-bold">Ocorreu um erro!</h1>
+        <h1 className="text-3xl fon-bold">Ocorreu um erro!o</h1>
         <h1 className="text-2xl fon-bold">Página  não encontrada.</h1>
     </div>
   )
