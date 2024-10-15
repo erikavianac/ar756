54eb6c3401e1a887c59a343ce619f7c4cf5c5f3a
Igor Goncalo <igorlabakig@gmail.com>
2024-10-15 15:54:11 +0100

    d

--- src/components/explore/galeria/galeriaList.tsx
+++ src/components/explore/galeria/galeriaList.tsx
@@ -42,7 +42,7 @@ export default function GaleriaListComponent({
     <Scrollbars
       style={{
         width: "100%",
-        height: 750,
+        height: 550,
         gap: 20,
       }}
     >
