53b5c9c03eca6e345882fd92e58ec0977faef8cd
Igor Goncalo <igorlabakig@gmail.com>
2024-10-21 12:44:09 +0100

    f

--- src/action/emailOrcamento.ts
+++ src/action/emailOrcamento.ts
@@ -34,13 +34,13 @@ export default async function sendOrcamentoEmail(
     subject: "Proposta AR756",
     html: `
            <div style="font-family: Arial, sans-serif; height: 990px;">
-              <table style="width: 100%; height: 100%; background-image: url('https://res.cloudinary.com/dzwboczzd/image/upload/v1727109726/piscina-cima_ga2yep.jpg'); background-size: cover;">
+              <table style="width: 100%; height: 100%; background-image: url('https://res.cloudinary.com/dzvyh5r33/image/upload/v1729000947/file_6_hrlmuh_rx6o37.jpg'); background-size: cover;">
                   <tr>
                       <td>
                       <table style="background-color: white; margin: auto; padding: 20px; width: 50%; height: 690px; border-radius: 10px;">
                           <tr>
                           <td style="text-align: center;">
-                              <img style="width: 200px; height: 120px; margin: 0 auto;" src="https://res.cloudinary.com/dzwboczzd/image/upload/v1727111537/WhatsApp_Image_2024-09-23_at_18.11.28_rbl0mo.jpg" alt="logo AR756" />
+                              <img style="width: 200px; height: 120px; margin: 0 auto;" src="https://res.cloudinary.com/dzvyh5r33/image/upload/v1729510932/WhatsApp_Image_2024-09-23_at_18.11.28_ivgong.jpg" alt="logo AR756" />
                               <h1 style="color: #333; width: 100%; text-align: center; margin-top: 10px;">Olá ${data.nome}, recebemos a sua mensagem!</h1>
                               <p style="font-size: 14px; width: 60%; margin: 10px auto; text-align: center;">Agradecemos o seu interesse em conhecer a AR756. Simulamos um orçamento para seu evento, por gentileza clique no botão abaixo para ver a proposta.</p>
                               <div style="width: 100%; text-align: center;margin-top: 40px;">
@@ -50,9 +50,9 @@ export default async function sendOrcamentoEmail(
                               </div>
                               <h2 style="font-size: 14px; width: 60%; margin: 10px auto; text-align: center; margin-top: 40px;">SIGA-NOS</h2>
                               <div style="text-align: center;">
-                              <a href="https://www.tiktok.com/@ar756_" style="text-decoration: none; margin-right: 10px;"><img src="https://res.cloudinary.com/dzwboczzd/image/upload/v1727110723/icons8-tiktok-48_eopzfz.png" alt="Logo TiTok" /></a>
-                              <a href="https://www.instagram.com/ar756_/" style="text-decoration: none; margin-right: 10px;"><img src="https://res.cloudinary.com/dzwboczzd/image/upload/v1727110723/icons8-instagram-48_x5ndr2.png" alt="Logo Instagram" /></a>
-                              <a href="https://www.facebook.com/profile.php?id=100085832906065" style="text-decoration: none;"><img src="https://res.cloudinary.com/dzwboczzd/image/upload/v1727110723/icons8-facebook-48_veyzdw.png" alt="Logo Facebook" /></a>
+                              <a href="https://www.tiktok.com/@ar756_" style="text-decoration: none; margin-right: 10px;"><img src="https://res.cloudinary.com/dzvyh5r33/image/upload/v1729510932/icons8-tiktok-48_to1o5t.png" alt="Logo TiTok" /></a>
+                              <a href="https://www.instagram.com/ar756_/" style="text-decoration: none; margin-right: 10px;"><img src="https://res.cloudinary.com/dzvyh5r33/image/upload/v1729510932/icons8-instagram-48_ezz3z6.png" alt="Logo Instagram" /></a>
+                              <a href="https://www.facebook.com/profile.php?id=100085832906065" style="text-decoration: none;"><img src="https://res.cloudinary.com/dzvyh5r33/image/upload/v1729510932/icons8-facebook-48_x31mos.png" alt="Logo Facebook" /></a>
                               </div>
                           </td>
                           </tr>
