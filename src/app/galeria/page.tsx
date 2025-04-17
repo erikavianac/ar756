ee3e7a94c7dcdf00a8e5f318b8b546daf3798cd2
Igor Goncalo <igorlabakig@gmail.com>
2025-04-17 09:59:57 +0100

    s

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -5,16 +5,17 @@ import { ImageRequestResponse } from "@/types";
 
 export const metadata: Metadata = {
   title: "Galeria",
+  metadataBase: new URL("https://ar756.com"),
 };
 
 export default async function Galeria() {
   const imageList = await fetch(
-    `${process.env.SERVER_URL}/image/list?venueId=8159e209-0057-4df3-ae72-855363c3b84e`
+    `${process.env.SERVER_URL}/image/getByTag?venueId=8159e209-0057-4df3-ae72-855363c3b84e`
   ).then(async (resp) => {
     const response: ImageRequestResponse = await resp.json();
     return response.data.imagesByTag;
   });
-
+  console.log(imageList)
   return (
     <div className="flex justify-center items-center pt-20">
       <div className="flex-1 mt-20">
