c6df7a74b8d40f25905c31d677df3d5164ef3b49
Igor Goncalo <igorlabakig@gmail.com>
2025-04-24 19:22:56 +0100

    d

--- next.config.mjs
+++ next.config.mjs
@@ -19,48 +19,29 @@ const nextConfig = {
         pathname: '**',
       }
     ],
-    minimumCacheTTL: 31536000, // 1 ano
+    minimumCacheTTL: 3600, // 1 hora
     deviceSizes: [640, 750, 1080, 1200, 1920],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    imageSizes: [32, 64, 96, 128, 256, 384],
   },
   env: {
     metadataBase: 'https://www.ar756.com/', // Defina a URL base do seu site aqui
   },
   reactStrictMode: true,
-  experimental: {
-    optimizeCss: true,
-    optimizeImages: true,
-    scrollRestoration: true,
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable, stale-while-revalidate=86400' },
-          { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
-        ],
-      },
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Cache-Control', value: 'public, max-age=3600, stale-while-revalidate=60' }
-        ],
-      },
-      {
-        source: '/_next/image/:path*',
-        headers: [
-          { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' }
-        ],
-      },
-    ];
-  },
+    async headers() {
+      return [
+        {
+          source: '/:path*',
+          headers: [
+            { key: 'Access-Control-Allow-Credentials', value: 'true' },
+            { key: 'Access-Control-Allow-Origin', value: '*' },
+            { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+            { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
+            { key: 'Cache-Control', value: 'public, max-age=3600, must-revalidate' },
+            { key: 'Strict-Transport-Security', value: 'max-age=63072000; includeSubDomains; preload' },
+          ],
+        },
+      ];
+    },
 };
 
 export default nextConfig;
