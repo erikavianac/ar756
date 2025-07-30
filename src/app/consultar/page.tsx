a7561a699b37f7982c9351f19521d7f95902c179
Igor Goncalo <igorlabakig@gmail.com>
2025-07-30 17:34:07 +0100

    refactor: remove unused services prop in ConsultarPage

--- src/app/consultar/page.tsx
+++ src/app/consultar/page.tsx
@@ -22,20 +22,12 @@ export const metadata: Metadata = {
 };
 
 export default async function ConsultarPage() {
-  const services = await fetch(
-    `${process.env.SERVER_URL}/service/list?venueId=8159e209-0057-4df3-ae72-855363c3b84e`,
-    {
-      method: "GET",
-    }
-  ).then(async (resp) => {
-    const response: ServiceRequestResponse = await resp.json();
-    return response?.data?.serviceList;
-  });
+
   return (
     <div className="flex flex-col min-h-screen w-full bg-faixada">
       <HomeHeaderComponent />
       <main className="flex-1 flex justify-center items-center w-full px-3 py-5">
-        <ConsultarFormComponent services={services} />
+        <ConsultarFormComponent />
       </main>
       <FooterComponent />
     </div>
